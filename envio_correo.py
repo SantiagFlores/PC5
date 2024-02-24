39ab488e03145d058e573e42406d650b01da8738
SantiagFlores <157295672+SantiagFlores@users.noreply.github.com>
2024-02-24 15:50:06 +0000

    PC

---
+++ envio_correo.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+def enviar_correo():
+    # Configuración del servidor SMTP y credenciales
+    smtp_server = 'tu_servidor_smtp'
+    smtp_port = 587
+    username = 'tu_correo@gmail.com'
+    password = 'tu_contraseña'
+
+    # Crear instancia de servidor SMTP
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(username, password)
+
+    # Configuración del correo
+    mensaje = MIMEMultipart()
+    mensaje['From'] = 'tu_correo@gmail.com'
+    mensaje['To'] = 'destinatario@example.com'
+    mensaje['Subject'] = 'Asunto del correo'
+
+    # Cuerpo del correo
+    cuerpo_correo = MIMEText('Texto del cuerpo del correo')
+    mensaje.attach(cuerpo_correo)
+
+    # Adjuntar archivo al correo
+    archivo_adjunto = MIMEApplication(open('archivo_adjunto.xlsx', 'rb').read())
+    archivo_adjunto.add_header('Content-Disposition', 'attachment', filename='archivo_adjunto.xlsx')
+    mensaje.attach(archivo_adjunto)
+
+    # Enviar correo
+    server.send_message(mensaje)
+
+    # Cerrar conexión
+    server.quit()
